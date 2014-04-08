a1c296510b377535d964cb52ae7bb9fd1fa2a397
Frank Shank <oak@Grahames-MacBook-Air.local>
2014-04-08 12:30:23 +0100

    farh-celc

--- ch1/temperature.c
+++ ch1/temperature.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-float celcius(float far);
+float celcius(float fahrenheit);
 
 /* print the Farenheight to Celcius scale */
 int main()
 {
-  printf("f:%6.2f\n", celcius(10.0));
+  int fahr;
+
+  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    printf("f:%d c:%6.2f\n", fahr, celcius(fahr));
   return 0;
 }
 
-/* given a farenheight float retunr the celcius value */
-float celcius(float far)
+/* given a farenheight value (float) return the celcius value */
+float celcius(float fahr)
 {
-  return 10.0;
+  return (5.0 / 9.0) * (fahr - 32);
 }
 
